0fdafedc916859c6e7f2b1bee72cb7d72a2f587f
unknown <nitin@belgiumwebnet.com>
2024-01-10 18:42:57 +0530

    first commit

---
+++ src/router/authentication.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+import { register } from '../controller/authentication';
+
+export default (router: express.Router) => {
+    router.post('/auth/register', register);
+};
+
